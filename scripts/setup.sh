20e65ef2275aa8c84e4f5a2c8f4ae48bbea77025
Miëtek Bak <mietek@bak.io>
2014-12-28 00:58:10 +0000

    Improve setup script

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -14,10 +14,11 @@ export SETUP_MONITOR_PORT='{{monitorPort}}'
 format_monitor () {
 	cat <<-'EOF'
 		#!/usr/bin/env bash
-		echo -e 'HTTP/1.1 200 OK\r'
+		echo -e 'HTTP/1.0 200 OK\r'
 		echo -e "Date: $( date -Ru )\r"
 		echo -e 'Access-Control-Allow-Origin: *\r'
-		echo -e 'Content-Type: text/plain; charset=utf-8\r\n\r'
+		echo -e 'Content-Type: text/plain; charset=utf-8\r'
+		echo -e 'Connection: close\r\n\r'
 		tail -n +0 -F '/var/log/setup.log' --pid="${SETUP_INTERNAL_STREAM_PID}"
 		echo -e '\r\n\r'
 EOF
@@ -52,7 +53,7 @@ install_halcyon () {
 	uid=$( id -u app ) || return 1
 	gid=$( id -g app ) || return 1
 
-	echo '-----> Welcome to Haskell on DigitalOcean' >'/var/log/setup.log'
+	echo '-----> Welcome to Haskell on DigitalOcean' >&2
 
 	( while true; do sleep 1; done ) &
 	export SETUP_INTERNAL_STREAM_PID="$!"
@@ -76,7 +77,7 @@ install_halcyon () {
 	packages+=( 'pigz' )
 	packages+=( 'zlib1g-dev' )
 
-	echo '-----> Installing OS packages' >>'/var/log/setup.log'
+	echo '-----> Installing OS packages' >&2
 
 	apt-get update || return 1
 	apt-get install -y "${packages[@]}" || return 1
@@ -89,7 +90,7 @@ install_halcyon () {
 		branch='master'
 	fi
 
-	echo -n '-----> Installing Halcyon...' >>'/var/log/setup.log'
+	echo -n '-----> Installing Halcyon...' >&2
 
 	local commit_hash
 	if ! commit_hash=$(
@@ -98,10 +99,10 @@ install_halcyon () {
 		git checkout -q "${branch}" >'/dev/null' 2>&1 &&
 		git log -n 1 --pretty='format:%h'
 	); then
-		echo 'error' >>'/var/log/setup.log'
+		echo 'error' >&2
 		return 1
 	fi
-	echo " done, ${commit_hash:0:7}" >>'/var/log/setup.log'
+	echo " done, ${commit_hash:0:7}" >&2
 	chown app:app -R '/app' || return 1
 
 	source <( HALCYON_NO_SELF_UPDATE=1 \
@@ -112,7 +113,7 @@ install_halcyon () {
 }
 
 
-if ! install_halcyon; then
+if ! install_halcyon >'/var/log/setup.log' 2>&1; then
 	echo '   *** ERROR: Failed to install Halcyon' >>'/var/log/setup.log'
 	exit 1
 fi
@@ -131,18 +132,18 @@ install_app () {
 	local commit_hash
 	if ! commit_hash=$( git_clone_over "${SETUP_APP_SOURCE_URL}" "${clone_dir}" ); then
 		log_end 'error'
-		die 'Failed to clone app'
+		log_error 'Failed to clone app'
+		return 1
 	fi
 	log_end "done, ${commit_hash:0:7}"
 
 	sudo -u app bash -c "
 		HOME='/app' \
 		HALCYON_NO_SELF_UPDATE=1 \
-			/app/halcyon/halcyon install \"${clone_dir}\" 2>&1 |
-				tee -a '/var/log/setup.log'
+			/app/halcyon/halcyon install \"${clone_dir}\"
 	" || return 1
 
-	kill "${SETUP_INTERNAL_STREAM_PID}" || return 1
+	kill "${SETUP_INTERNAL_STREAM_PID}" || true
 	( sleep "${SETUP_MONITOR_LIFE:-3600}" && kill "${SETUP_INTERNAL_MONITOR_PID}" ) &
 
 	local app_command
@@ -153,7 +154,7 @@ install_app () {
 			sudo -u app bash -c "
 				HOME='/app' \
 				HALCYON_NO_SELF_UPDATE=1 \
-					/app/halcyon/halcyon executable "${clone_dir}" 2>'/dev/null'
+					/app/halcyon/halcyon executable \"${clone_dir}\" 2>'/dev/null'
 			"
 		); then
 			expect_existing "/app/bin/${executable}"
@@ -162,14 +163,28 @@ install_app () {
 		fi
 	fi
 	if [[ -z "${app_command}" ]]; then
-		log_warning 'Cannot create Upstart config' 2>'/var/log/setup.log'
+		log_warning 'Cannot create Upstart config'
 		return 0
 	fi
 
 	format_upstart_config "${app_command}" >'/etc/init/app.conf' || return 1
-
 	start app || return 1
+
+	local ip_address
+	ip_address=$( curl http://169.254.169.254/metadata/v1/interfaces/public/0/ipv4/address ) || return 1
+
+	log
+	log
+	log 'Install succeeded'
+	log
+	log 'To see the app:'
+	log_indent "$ curl http://${ip_address}:${SETUP_APP_PORT:8080}"
+	log
+	log
 }
 
 
-install_app
+if ! install_app 2>>'/var/log/setup.log'; then
+	log_error 'Failed to install app' 2>>'/var/log/setup.log'
+	exit 1
+fi
